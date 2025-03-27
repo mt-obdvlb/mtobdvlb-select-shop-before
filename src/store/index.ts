65113efef566e7372c38ddee87d907efff83a57a
mtobdvlb <mtobdvlb@163.com>
2025-03-27 11:13:10 +0800

    pinia持久化

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,7 @@
 import { createPinia } from "pinia"
+import piniaPluginPersistenceOptions from "pinia-plugin-persistedstate"
 
 export const pinia = createPinia()
+pinia.use(piniaPluginPersistenceOptions)
 
 export default pinia
--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -4,18 +4,27 @@ import type { loginFormData } from "../../api/user/type";
 
 const useUserStore = defineStore("User", {
   state: () => {
-    return {}
+    return {
+      token: '',
+    }
   },
   actions: {
     async userLogin (data: loginFormData) {
       const res = await reqLogin(data)
-      console.log(res);
-      
+      if (res.code === 200) {
+        // 登录成功
+        this.token = res.data.token
+        console.log(this.token);
+        
+      } else {
+        
+      }
     }
   },
   getters: {
 
-  }
+  },
+  persist: true
 })
 
 export default useUserStore
