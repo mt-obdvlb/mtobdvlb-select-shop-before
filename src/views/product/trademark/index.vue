3bb4c0184191e177fef7a4d9ce4fce2ab20efff9
mtobdvlb <mtobdvlb@163.com>
2025-04-02 12:00:09 +0800

    feat(product/trademark): 实现商标分页功能并优化数据获取
    - 在 el-pagination 组件中添加 total 属性，用于显示总数据数
    - 在 getHasTrademark 函数中更新接口请求，获取正确的总数据数
    - 优化数据结构，使用更准确的类型定义

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,44 +1,71 @@
 <template>
-  <el-card>
-    <el-button type="primary" icon="Plus"
-    >添加品牌</el-button>
-    <el-table border
-              :data="trademarkList"
+  <div>
+    <el-card>
+      <el-button type="primary" icon="Plus" @click="addTrademark"
+      >添加品牌</el-button>
+      <el-table border
+                :data="trademarkList"
 
-    >
-      <el-table-column label="序号" width="80" align="center" type="index"></el-table-column>
-      <el-table-column label="品牌名称" prop="tmName">
-        <template #="{row, $index}">
-          <pre>{{row.tmName}}</pre>
-        </template>
+      >
+        <el-table-column label="序号" width="80" align="center" type="index"></el-table-column>
+        <el-table-column label="品牌名称" prop="tmName">
+          <template #="{row, $index}">
+            <pre>{{row.tmName}}</pre>
+          </template>
 
-      </el-table-column>
-      <el-table-column label="品牌Logo">
-        <template #="{row, $index}">
-          <el-image :src="row.logoUrl"></el-image>
-        </template>
-      </el-table-column>
-      <el-table-column label="品牌仓库">
-        <template #="{row, $index}">
-          <el-button type="primary" icon="Edit"
-          ></el-button>
-          <el-button type="danger" icon="Delete"
-          ></el-button>
-        </template>
-      </el-table-column>
-    </el-table>
-    <el-pagination
-        v-model:current-page="pageNo"
-        v-model:page-size="limit"
-        :page-sizes="[3,6,9]"
-        :total="total"
-        background
-        layout="prev, pager, next, jumper, ->, sizes, total"
-        @change="getHasTrademark"
-    >
+        </el-table-column>
+        <el-table-column label="品牌Logo">
+          <template #="{row, $index}">
+            <el-image :src="row.logoUrl"></el-image>
+          </template>
+        </el-table-column>
+        <el-table-column label="品牌仓库">
+          <template #="{row, $index}">
+            <el-button type="primary" icon="Edit" @click="updateTrademark"
+            ></el-button>
+            <el-button type="danger" icon="Delete"
+            ></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+      <el-pagination
+          v-model:current-page="pageNo"
+          v-model:page-size="limit"
+          :page-sizes="[3,6,9]"
+          :total="total"
+          background
+          layout="prev, pager, next, jumper, ->, sizes, total"
+          @change="getHasTrademark"
+      >
 
-    </el-pagination>
-  </el-card>
+      </el-pagination>
+    </el-card>
+    <el-dialog title="添加品牌" v-model="dialogVisible">
+      <el-form
+          style="width: 80%"
+      >
+        <el-form-item label="品牌名称"  label-width="80px">
+          <el-input v-model="trademarkName" placeholder="请输入品牌名称"></el-input>
+        </el-form-item>
+        <el-form-item label="品牌Logo" label-width="80px">
+          <el-upload
+              class="avatar-uploader"
+              action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
+              :show-file-list="false"
+              :on-success="handleAvatarSuccess"
+              :before-upload="beforeAvatarUpload"
+          >
+            <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <el-button type="primary" @click="confirm">确定</el-button>
+        <el-button @click="cancel">取消</el-button>
+      </template>
+    </el-dialog>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -47,6 +74,7 @@ import {reqHasTrademark} from "@/api/product/trademark";
 import {onMounted} from "vue";
 import type {Records, TradeMarkResponseData} from "@/api/product/trademark/type.ts";
 
+let dialogVisible = ref<boolean>(false);
 let trademarkList = ref<Records>([]);
 let pageNo = ref<number>(1);
 let limit = ref<number>(3);
@@ -61,6 +89,21 @@ const getHasTrademark = async () => {
   }
 }
 
+const addTrademark = () => {
+  dialogVisible.value = true;
+}
+
+const updateTrademark = () => {
+  dialogVisible.value = true;
+}
+
+const confirm = () => {
+  dialogVisible.value = false;
+}
+
+const cancel = () => {
+  dialogVisible.value = false;
+}
 
 onMounted(  () => {
   getHasTrademark()
@@ -68,6 +111,26 @@ onMounted(  () => {
 
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
+}
 
 </style>
